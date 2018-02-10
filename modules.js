511c59b4ec8d0c141362066a9bf3871976a60e0d
Pascal Rhod <pascal.rhod@canaltp.fr>
2018-02-10 16:37:49 +0100

    refacto display

--- modules.js
+++ modules.js
@@ -64,3 +64,4 @@ function get_parameter_from_url(param_name) {
         results = regex.exec(location.href);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
